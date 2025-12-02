122b32ea4632a165f2b64618798a5cb4b0ad8121
mahmoud <software@a2tech.my>
2025-12-02 12:22:21 +0800

    update pkg name

--- launch/xsens_mti_node.launch.py
+++ launch/xsens_mti_node.launch.py
@@ -12,9 +12,9 @@ def generate_launch_description():
     arg = SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
     ld.add_action(arg)
 
-    parameters_file_path = Path(get_package_share_directory('ros2_xsens_mti_driver'), 'param', 'xsens_mti_node.yaml')
+    parameters_file_path = Path(get_package_share_directory('dhas_imu_xsens'), 'param', 'xsens_mti_node.yaml')
     xsens_mti_node = Node(
-            package='ros2_xsens_mti_driver',
+            package='dhas_imu_xsens',
             executable='xsens_mti_node',
             name='xsens_mti_node',
             output='screen',
